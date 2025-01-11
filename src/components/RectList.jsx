daf15f3b46316bd25029290e02cafcfc7564cdc5
Marcus Fu <38443839+Ecodelis@users.noreply.github.com>
2025-01-10 17:17:52 -0800

    projects props are now variable for each component

--- src/components/RectList.jsx
+++ src/components/RectList.jsx
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import RectDisplay from './RectDisplay.jsx';
 
-const projects = [
-  { type: 'PROJECT', title: 'CAR PROJ', description: 'Description of Project 1', startDate: 'Jan 2021', endDate: 'Dec 2021', numPeople: '5 People' },
-  { type: 'PROJECT', title: 'CAR PROJ', description: 'Description of Project 2', startDate: 'Feb 2021', endDate: 'Dec 2021', numPeople: '4 People' },
-  { type: 'PROJECT', title: 'CAR PROJ', description: 'Description of Project 3', startDate: 'Mar 2021', endDate: 'Dec 2021', numPeople: '3 People' },
-  { type: 'PROJECT', title: 'CAR PROJ', description: 'Description of Project 4', startDate: 'Apr 2021', endDate: 'Dec 2021', numPeople: '2 People' },
-  { type: 'PROJECT', title: 'CAR PROJ', description: 'Description of Project 5', startDate: 'May 2021', endDate: 'Dec 2021', numPeople: '1 Person' },
-];
-
-const ProjectsList = () => {
+const RectList = ({ headerTitle, projects }) => {
   const [showAll, setShowAll] = useState(false);
-  const visibleProjects = showAll ? projects : projects.slice(0, 4);
+  const visibleProjects = showAll ? projects : projects.slice(0, 3);
 
   useEffect(() => {
-    console.log('Rendering ProjectsList component');
+    console.log('Rendering RectList component');
     console.log('Current showAll state:', showAll);
   }, [showAll]);
 
@@ -26,7 +18,7 @@ const ProjectsList = () => {
   return (
     <div className="carousel-container flex flex-col items-center py-12 px-10 max-w-7xl mx-auto box-border">
       <h1 className="text-[1rem] mb-10 text-white text-left w-full relative">
-        Recent Projects
+        {headerTitle}
         <span className="absolute bottom-0 left-0 w-full h-[2px] bg-white"></span>
       </h1>
       
@@ -43,4 +35,4 @@ const ProjectsList = () => {
   );
 };
 
-export default ProjectsList;
+export default RectList;
