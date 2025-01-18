f8bd7bf0161a6c34a5114a31afcc9d8a04e73dda
Ecodelis <38443839+Ecodelis@users.noreply.github.com>
2025-01-18 02:44:15 -0800

    Created Nav.astro component - Customizable with navData.js - Not styled yet

---
+++ src/data/navData.js
@@ -0,0 +1,33 @@
+// Home will be linked to and ID and have an ICON. (auto scrolling + icon)
+// Projects, Competitions, Experiences, About will be linked to an ID. (auto scrolling)
+// Resume will be linked to a URL going to another page.
+
+const navData = [
+    {
+        name: "Home",
+        path: "#home",
+        icon: "logomark"
+    },
+    {
+        name: "Projects",
+        path: "#projects"
+    },
+    {
+        name: "Competitions",
+        path: "#competitions"
+    },
+    {
+        name: "Experiences",
+        path: "#experiences"
+    },
+    {
+        name: "About",
+        path: "#about"
+    },
+    {
+        name: "Resume",
+        path: "/resume/"
+    }
+];
+
+export default navData;
